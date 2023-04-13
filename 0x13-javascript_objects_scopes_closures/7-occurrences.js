429fd59d5e8eea082887848f0cb139e1d4582cf0
tinashenatty <nattymachona2000@gmail.com>
2023-04-13 11:35:42 -0700

    Task on object scopes[ALL]

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+exports.nbOccurences = function (list, searchElement) {
+  return list.reduce((count, val) => {
+    if (val === searchElement) {
+      count++;
+    }
+    return count;
+  }, 0);
+};
+Footer
+
