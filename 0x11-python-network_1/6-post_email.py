00a96b0b191c871195a18f4fff000a99ad46ec83
tinashenatty <nattymachona2000@gmail.com>
2023-06-30 16:51:54 -0700

    POST an email #1

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""sends a POST request to the passed URL with the email as a
+parameter, and finally displays the body of the response."""
+if __name__ == '__main__':
+    import requests
+    import sys
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data = {'email': email}
+    response = requests.post(url, data=data)
+    print(response.text)
